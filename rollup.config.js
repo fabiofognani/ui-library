d6f124a8a91ff949a441dad3d329af87102eefc2
fabiofognani <ffognani@florence-consulting.it>
2023-06-09 10:31:09 +0200

    added tests

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 import PackageJSON from "./package.json" assert { type: "json" };
 
 export default [
@@ -21,14 +25,20 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];

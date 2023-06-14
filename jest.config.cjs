7e75225e1c2d4da78243b034abe0be55b7a46437
fabiofognani <ffognani@florence-consulting.it>
2023-06-14 11:30:38 +0200

    fix: tests preset

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: "jsdom",
-  preset: "ts-jest",
   moduleNameMapper: {
     ".(css|less|scss)$": "identity-obj-proxy",
     ".(ttf|eot|woff|woff2)$": "identity-obj-proxy",
